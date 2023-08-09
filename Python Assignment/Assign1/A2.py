0012fb04769d8216c90c96590eabe6a5c54ff333
Sumant Pujari <90272569+Anonymous-988@users.noreply.github.com>
2023-08-09 15:49:36 +0530

    Deleted duplicate Directory

--- Python Assignment/Assign1/A2.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-num = int(sys.argv[1])
-if num<=50 and num>=1:
-    print("Ok")
-else:
-    print("Out of Range")
--- Python Assignment/Assign1/B2.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Enter a number: "))
-
-for i in range(num+1):
-    for j in range(num - i):
-        print(j+1, end="\t")
-    print()
