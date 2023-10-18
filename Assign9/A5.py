cf78d04da1b4a1963a5e380e393bdebee92c822e
Sumant Pujari <90272569+Anonymous-988@users.noreply.github.com>
2023-10-18 08:58:41 +0530

    Updated as of 10182023
    Updated the menu driven program for Student Management Program

--- Assign9/A5.py
+++ Assign9/A5.py
@@ -6,7 +6,17 @@ class Student:
         self.percentage = 0
 
     def calculatePercentage(self):
-        totalMarks = self.
+        totalMarks = self.subMark[0][1] + self.subMark[1][1] + self.subMark[2][1]
+        self.percentage = (totalMarks / 3)
+        self.percentage = round(self.percentage,2)
+
+    def studentDetails(self):
+        print(f"\nStudent Name: {self.name}")
+        print(f"Student Name: {self.rollno}")
+        print(f"Subject Details are as: ")
+        for subName, subMark in self.subMark:
+            print(f"\t{subName} -> {subMark}")
+        print(f"Overall Percentage: {self.percentage}")
 
 
 students = {}
@@ -38,9 +48,23 @@ while True:
         if rollNo in students:
             obj = students.pop(rollNo)
             del obj
+            print("Student {rollNo} deleted successfully.")
+        else:
+            print("Student {rollNo} not found.")
 
     elif choice == "3":
+        if len(students.values()) == 0:
+            print("No Student details available")
+            continue
 
+        print("Student Details are as below: ")
+        for studentObj in students.values():
+            studentObj.calculatePercentage()
+            studentObj.studentDetails()
+        
+    
+    elif choice == "4":
+        break
 
-
-## Have to implement proper methods to the Student Class as well need to reconstruct the switch cases
+    else:
+        print("Wrong Input")
